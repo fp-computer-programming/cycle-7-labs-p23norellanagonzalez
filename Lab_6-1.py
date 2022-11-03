fa9b6f156f7c75c769712ab160121aecf29e239c
Nickolas Orellana <p23norellanagonzalez@fairfieldprep.org>
2022-11-03 11:15:58 -0400

    done

---
+++ Lab_6-1.py
@@ -0,0 +1,21 @@
+#author NDO 11/03/22
+
+#create a arrray that contains a number list 1-5 called my_arr
+my_arr = [1,2,3,4,5]
+
+# write a statement indexing the number 3
+num_three = my_arr.index(4)
+print(num_three)
+
+# write a statement that prints the length of the list
+x =len(my_arr)
+print(x)
+
+#write a statement that repeats the list 3 times
+my_arr3 = [1,2,3,4,5] * 3
+print(my_arr3)
+
+#write a statemeent that converts a string into a array 
+y = 'string'
+z = list(y)
+print(z)
